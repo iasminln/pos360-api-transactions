e99006dc40a0abd3dfa5596d9304e8feaa43302e
Iasmin <iasln@outlook.com>
2025-03-30 16:36:54 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  // const transactions = await knex('transactions').insert({
+  //   id: crypto.randomUUID(),
+  //   title: 'Transação de teste',
+  //   amount: 1000
+  // }).returning('*')
+
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
+})
+
+app.listen({
+  port: env.PORT
+}).then(() => {
+  console.log('HTTP Server Running!')
+})
